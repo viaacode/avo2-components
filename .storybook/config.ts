ff70c07e6660abf5c8636d25adcd0840a30c8719
Enzo Eghermanne <enzo.eghermanne@gmail.com>
2019-05-22 13:39:30 +0200

    feat: added knobs addon

--- .storybook/config.ts
+++ .storybook/config.ts
@@ -1,11 +1,15 @@
 import { withTests } from '@storybook/addon-jest';
+import { withKnobs } from '@storybook/addon-knobs';
 import { addDecorator, configure } from '@storybook/react';
 import { jsxDecorator } from 'storybook-addon-jsx';
 import { withPropsTable } from 'storybook-addon-react-docgen';
+import { withSmartKnobs } from 'storybook-addon-smart-knobs';
 
 import results from './test-results.json';
 
 addDecorator(withTests({ results, filesExt: '((\\.specs?)|(\\.tests?))?(\\.tsx)?$' }));
+addDecorator(withKnobs);
+addDecorator(withSmartKnobs);
 addDecorator(withPropsTable);
 addDecorator(jsxDecorator);
 
