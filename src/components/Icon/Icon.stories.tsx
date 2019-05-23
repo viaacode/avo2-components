626d7ade0ed91f99b1bb8204bad0b044cdad99ff
Enzo Eghermanne <enzo.eghermanne@gmail.com>
2019-05-23 15:03:26 +0200

    feat: added stories for icons

--- src/components/Icon/Icon.stories.tsx
+++ src/components/Icon/Icon.stories.tsx
@@ -2,37 +2,43 @@ import React, { Fragment } from 'react';
 
 import { storiesOf } from '@storybook/react';
 
-import * as icons from '../Icons';
+import iconList from '../../icons/icons.json';
+
 import { Icon } from './Icon';
 
-storiesOf('Icons', module)
-	.add('multicolor', () => (
+const baseIcons = iconList.filter(i => i.type === '');
+const arrowsIcons = iconList.filter(i => i.type === 'arrows');
+const customIcons = iconList.filter(i => i.type === 'custom');
+const multicolorIcons = iconList.filter(i => i.type === 'multicolor');
+const socialIcons = iconList.filter(i => i.type === 'social');
+const wysiwygIcons = iconList.filter(i => i.type === 'wysiwyg');
+
+const stories = [
+	['Base', baseIcons],
+	['Arrows', arrowsIcons],
+	['Custom', customIcons],
+	['Multicolor', multicolorIcons],
+	['social', socialIcons],
+	['WYSIWYG', wysiwygIcons],
+	['All icons', iconList],
+];
+
+stories.forEach(([title, icons]: any) =>
+	storiesOf('Icons', module).add(title, () => (
 		<Fragment>
-			<h1>Multicolor</h1>
+			<h1>{title}</h1>
 			<div className="c-styleguide-svg-icons__category">
-				{Object.keys(icons).map(name => (
-					<div className="c-styleguide-svg-icon">
-						<Fragment>
-							<Icon name={name} />
-							<code>{name}</code>
-						</Fragment>
-					</div>
-				))}
+				<div className="c-styleguide-svg-icons__type">
+					{icons.map(({ name, type }: { name: string; type: any }) => (
+						<div className="c-styleguide-svg-icon">
+							<Fragment>
+								<Icon name={name} type={type} />
+								<code>{name}</code>
+							</Fragment>
+						</div>
+					))}
+				</div>
 			</div>
 		</Fragment>
 	))
-	.add('All icons', () => (
-		<Fragment>
-			<h1>All icons</h1>
-			<div className="c-styleguide-svg-icons__category">
-				{Object.keys(icons).map(name => (
-					<div className="c-styleguide-svg-icon">
-						<Fragment>
-							<Icon name={name} />
-							<code>{name}</code>
-						</Fragment>
-					</div>
-				))}
-			</div>
-		</Fragment>
-	));
+);
--- src/components/Icon/Icon.tsx
+++ src/components/Icon/Icon.tsx
@@ -10,15 +10,23 @@ export interface IconProps {
 }
 
 export const Icon: FunctionComponent<IconProps> = ({ name, type }: IconProps) => {
-	const NamedIcon = (Icons as any)[name];
+	const NamedIcon = (Icons as any)[toPascalCase(name)];
 
 	function getIconName() {
 		const base = 'o-svg-icon';
 		const iconName = toKebabCase(name);
 
-		return `${base}-${type}-${iconName}`;
+		return `${base}-${type || ''}-${iconName}`;
 	}
 
+	// converts kebab-case to PascalCase
+	function toPascalCase(string: string) {
+		return string
+			.replace(/(^\w)/, first => first.toUpperCase())
+			.replace(/(\-\w)/g, match => match[1].toUpperCase());
+	}
+
+	// converts PascalCase to kebab-case
 	function toKebabCase(string: string) {
 		return string
 			.replace(/([a-z0-9])([A-Z])/g, '$1-$2')
