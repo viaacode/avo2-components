a979910b8d1c8821962e371136b9738493be33ea
Enzo Eghermanne <enzo.eghermanne@gmail.com>
2019-05-27 12:55:17 +0200

    refactor: <AvatarWithText> into <Avatar>

--- src/components/Avatar/Avatar.tsx
+++ src/components/Avatar/Avatar.tsx
@@ -2,11 +2,27 @@ import React, { FunctionComponent } from 'react';
 
 import classNames from 'classnames';
 
-export interface AvatarProps {
-	initials: string;
-	size?: 'small';
+import { AvatarIcon, AvatarIconProps } from './AvatarIcon';
+
+export interface AvatarProps extends AvatarIconProps {
+	name?: string;
+	title?: string;
 }
 
-export const Avatar: FunctionComponent<AvatarProps> = ({ initials, size }: AvatarProps) => (
-	<div className={classNames('c-avatar', { 'c-avatar--small': size === 'small' })}>{initials}</div>
-);
+export const Avatar: FunctionComponent<AvatarProps> = ({
+	initials,
+	size,
+	name,
+	title,
+}: AvatarProps) =>
+	name || title ? (
+		<div className="c-avatar-and-text">
+			<AvatarIcon initials={initials} size={size} />
+			<div className="c-avatar-and-text__text">
+				{name && <p>{name}</p>}
+				{title && <p className="u-text-muted">{title}</p>}
+			</div>
+		</div>
+	) : (
+		<AvatarIcon initials={initials} size={size} />
+	);
