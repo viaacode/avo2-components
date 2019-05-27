f14190949d6d2c958529d7532fb7094b05fd4c61
Enzo Eghermanne <enzo.eghermanne@gmail.com>
2019-05-27 12:56:47 +0200

    fix: pass <Avatar/> image down to <AvatarIcon/>

--- src/components/Avatar/Avatar.tsx
+++ src/components/Avatar/Avatar.tsx
@@ -12,17 +12,18 @@ export interface AvatarProps extends AvatarIconProps {
 export const Avatar: FunctionComponent<AvatarProps> = ({
 	initials,
 	size,
+	image,
 	name,
 	title,
 }: AvatarProps) =>
 	name || title ? (
 		<div className="c-avatar-and-text">
-			<AvatarIcon initials={initials} size={size} />
+			<AvatarIcon initials={initials} size={size} image={image} />
 			<div className="c-avatar-and-text__text">
 				{name && <p>{name}</p>}
 				{title && <p className="u-text-muted">{title}</p>}
 			</div>
 		</div>
 	) : (
-		<AvatarIcon initials={initials} size={size} />
+		<AvatarIcon initials={initials} size={size} image={image} />
 	);
