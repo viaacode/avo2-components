751adb0e828189100e60ca63aede9b5c70859a9d
Enzo Eghermanne <enzo.eghermanne@gmail.com>
2019-05-28 12:54:46 +0200

    feat: added <Badge> stories

---
+++ src/components/Badge/Badge.stories.tsx
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react';
+
+import { storiesOf } from '@storybook/react';
+
+import { Badge } from './Badge';
+
+storiesOf('Badge', module)
+	.addParameters({ jest: ['Badge'] })
+	.add('Badge', () => <Badge text="Pending approval" />)
+	.add('Success badge', () => <Badge text="Pending approval" type="success" />)
+	.add('Error badge', () => <Badge text="Pending approval" type="error" />);
