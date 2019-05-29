087fefd0bd282ebfa9cb07a701bd1ec6fc2c5be7
Enzo Eghermanne <enzo.eghermanne@gmail.com>
2019-05-29 09:58:04 +0200

    feat: added <Button /> tests

---
+++ src/components/Button/Button.test.tsx
@@ -0,0 +1,91 @@
+import { shallow } from 'enzyme';
+import React, { Fragment } from 'react';
+
+import { Icon } from '../Icon/Icon';
+
+import { Button } from './Button';
+
+describe('<Button />', () => {
+	it('Should be able to render', () => {
+		shallow(<Button />);
+	});
+
+	it('Should set the correct className', () => {
+		const buttonComponent = shallow(<Button />);
+
+		expect(buttonComponent.hasClass('c-button')).toEqual(true);
+	});
+
+	it('Should set the correct size className', () => {
+		const buttonComponent = shallow(<Button size="small" />);
+
+		expect(buttonComponent.hasClass('c-button--small')).toEqual(true);
+	});
+
+	it('Should set the correct type className', () => {
+		const secondaryButtonComponent = shallow(<Button type="secondary" />);
+		const dangerButtonComponent = shallow(<Button type="danger" />);
+		const linkButtonComponent = shallow(<Button type="link" />);
+
+		expect(secondaryButtonComponent.hasClass('c-button--secondary')).toEqual(true);
+		expect(dangerButtonComponent.hasClass('c-button--danger')).toEqual(true);
+		expect(linkButtonComponent.hasClass('c-button--link')).toEqual(true);
+	});
+
+	it('Should be able to render a label', () => {
+		const label = 'Click me!';
+		const buttonComponent = shallow(<Button label={label} />);
+
+		const labelElement = buttonComponent.find('.c-button__label');
+
+		expect(labelElement).toHaveLength(1);
+		expect(labelElement.text()).toEqual(label);
+	});
+
+	it('Should not to render a label when none is passed', () => {
+		const buttonComponent = shallow(<Button />);
+
+		const labelElement = buttonComponent.find('.c-button__label');
+
+		expect(labelElement).toHaveLength(0);
+	});
+
+	it('Should be able to render a label and an icon', () => {
+		const label = 'Click me!';
+		const icon = 'search';
+		const buttonComponent = shallow(<Button label={label} icon={icon} />);
+
+		const labelElement = buttonComponent.find('.c-button__label');
+		const iconComponent = buttonComponent.find(Icon);
+
+		expect(labelElement).toHaveLength(1);
+		expect(labelElement.text()).toEqual(label);
+		expect(iconComponent).toHaveLength(1);
+		expect(iconComponent.props()).toMatchObject({ name: icon });
+	});
+
+	it('Should get a special className when only an icon is passed', () => {
+		const buttonComponent = shallow(<Button icon="link" />);
+
+		expect(buttonComponent.hasClass('c-button--icon')).toEqual(true);
+	});
+
+	it('Should be able to render an arrow at the end of the button', () => {
+		const buttonComponent = shallow(<Button arrow />);
+
+		const arrowComponent = buttonComponent.find(Icon);
+
+		expect(arrowComponent).toHaveLength(1);
+		expect(arrowComponent.props()).toMatchObject({ name: 'caret-down' });
+	});
+
+	it('Should call the `onClick`-handler when clicked', () => {});
+
+	it('Should pass on the `disabled`-attribute', () => {
+		const buttonComponent = shallow(<Button disabled />);
+
+		const buttonElement = buttonComponent.find('button');
+
+		expect(buttonElement.prop('disabled')).toEqual(true);
+	});
+});
