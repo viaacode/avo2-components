22269ee211e134776e05582ecadd17e729c5ba4f
Enzo Eghermanne <enzo.eghermanne@gmail.com>
2019-05-31 15:30:44 +0200

    feat: added <FormGroup>

---
+++ src/components/Form/FormGroup.tsx
@@ -0,0 +1,29 @@
+import React, { FunctionComponent, ReactNode } from 'react';
+
+import classNames from 'classnames';
+
+export interface FormGroupProps {
+	key: string;
+	label?: string;
+	error?: string;
+	children: ReactNode;
+}
+
+export const FormGroup: FunctionComponent<FormGroupProps> = ({
+	key,
+	label,
+	error,
+	children,
+}: FormGroupProps) => (
+	<div className={classNames('o-form-group', { 'o-form-group--error': error })}>
+		{label && (
+			<label className="o-form-group__label" htmlFor={key}>
+				{label}
+			</label>
+		)}
+		<div className="o-form-group__controls">
+			{children}
+			{error && <div className="c-form-help-text c-form-help-text--error">{error}</div>}
+		</div>
+	</div>
+);
