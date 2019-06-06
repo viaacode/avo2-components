a0942dfbfdb36b13e8023081c030acd8cd2601fd
Enzo Eghermanne <enzo.eghermanne@gmail.com>
2019-06-06 16:15:32 +0200

    feat: added Modal stories

---
+++ src/components/Modal/Modal.stories.tsx
@@ -0,0 +1,79 @@
+import React, { Fragment, useState } from 'react';
+
+import { storiesOf } from '@storybook/react';
+
+import { Button } from '../Button/Button';
+
+import { Modal } from './Modal';
+
+storiesOf('Modal', module)
+	.addParameters({ jest: ['Modal'] })
+	.add('Modal', () => (
+		<Modal
+			isOpen={true}
+			title="Modal title"
+			renderOptions={() => (
+				<Fragment>
+					<Button type="secondary" label="Cancel" />
+					<Button type="primary" label="Ok" />
+				</Fragment>
+			)}
+		>
+			<p>
+				It doesn't matter where this modal occurs in the source code, React automatically moves it
+				to the root.
+			</p>
+		</Modal>
+	))
+	.add('Small modal', () => (
+		<Modal
+			isOpen={true}
+			title="Modal title"
+			size="small"
+			renderOptions={() => <Button type="primary" label="Ok" />}
+		>
+			<p>
+				It doesn't matter where this modal occurs in the source code, React automatically moves it
+				to the root.
+			</p>
+		</Modal>
+	))
+	.add('Medium modal', () => (
+		<Modal
+			isOpen={true}
+			title="Modal title"
+			size="medium"
+			renderOptions={() => <Button type="primary" label="Ok" />}
+		>
+			<p>
+				It doesn't matter where this modal occurs in the source code, React automatically moves it
+				to the root.
+			</p>
+		</Modal>
+	))
+	.add('Fullscreen modal', () => (
+		<Modal
+			isOpen={true}
+			title="Modal title"
+			size="fullscreen"
+			renderOptions={() => <Button type="primary" label="Ok" />}
+		>
+			<p>
+				It doesn't matter where this modal occurs in the source code, React automatically moves it
+				to the root.
+			</p>
+		</Modal>
+	))
+	.add('Auto-height modal', () => (
+		<Modal
+			isOpen={true}
+			title="Modal title"
+			size="auto"
+			renderOptions={() => <Button type="primary" label="Ok" />}
+		>
+			<p>
+				It doesn't matter where this modal occurs in the source code, React automatically moves it
+				to the root.
+			</p>
+		</Modal>
+	));
