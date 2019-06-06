7ab1d8f44ddd4a2182cb9a94929157b9e68d4422
Enzo Eghermanne <enzo.eghermanne@gmail.com>
2019-06-06 16:42:23 +0200

    feat: added ModalBackdrop-tests

--- src/components/Modal/Modal.stories.tsx
+++ src/components/Modal/Modal.stories.tsx
@@ -7,7 +7,7 @@ import { Button } from '../Button/Button';
 import { Modal } from './Modal';
 
 storiesOf('Modal', module)
-	.addParameters({ jest: ['Modal'] })
+	.addParameters({ jest: ['Modal', 'ModalBackdrop'] })
 	.add('Modal', () => (
 		<Modal
 			isOpen={true}
