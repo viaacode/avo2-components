8d7a8969cf5c89571705065cef8eedbd1c6a6b74
Enzo Eghermanne <enzo.eghermanne@gmail.com>
2019-06-07 16:46:06 +0200

    fix: use action from helpers ifo. @storybook/addon-actions

--- src/components/Checkbox/Checkbox.stories.tsx
+++ src/components/Checkbox/Checkbox.stories.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
 
-import { action } from '@storybook/addon-actions';
 import { storiesOf } from '@storybook/react';
+
+import { action } from '../../helpers/action';
 
 import { Checkbox } from './Checkbox';
 
 storiesOf('Checkbox', module)
 	.addParameters({ jest: ['Checkbox'] })
-	.add('Checkbox', () => <Checkbox label="One" id="one" onChanged={action('Checkbox toggled')} />)
+	.add('Checkbox', () => <Checkbox label="One" onChanged={action('Checkbox toggled')} />)
 	.add('Checkbox default checked', () => (
-		<Checkbox label="One" id="one" defaultChecked={true} onChanged={action('Checkbox toggled')} />
+		<Checkbox label="One" defaultChecked={true} onChanged={action('Checkbox toggled')} />
 	))
 	.add('Checkbox default unchecked', () => (
-		<Checkbox label="One" id="one" defaultChecked={false} onChanged={action('Checkbox toggled')} />
+		<Checkbox label="One" defaultChecked={false} onChanged={action('Checkbox toggled')} />
 	));
