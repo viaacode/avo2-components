d3b949b4d6180e90db9e20ff41c2d813a77f83a1
Enzo Eghermanne <enzo.eghermanne@gmail.com>
2019-06-11 13:38:30 +0200

    feat: added extra stories for TextInput

--- src/components/TextInput/TextInput.stories.tsx
+++ src/components/TextInput/TextInput.stories.tsx
@@ -15,6 +15,24 @@ storiesOf('TextInput', module)
 			<TextInput disabled placeholder="No typing here..." />
 		</Fragment>
 	))
+	.add('TextInput without placeholder', () => (
+		<Fragment>
+			<TextInput onChange={action('onChange')} />
+			<div className="u-spacer-bottom" />
+			<TextInput disabled />
+		</Fragment>
+	))
+	.add('TextInput with default value', () => (
+		<Fragment>
+			<TextInput
+				placeholder="And this is the placeholder"
+				defaultValue="This is the default value"
+				onChange={action('onChange')}
+			/>
+			<div className="u-spacer-bottom" />
+			<TextInput disabled defaultValue="This is the default value" />
+		</Fragment>
+	))
 	.add('TextInput with icon', () => (
 		<Fragment>
 			<TextInput icon="search" placeholder="Type something here..." onChange={action('onChange')} />
