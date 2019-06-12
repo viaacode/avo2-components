6661440ad3eb8a7c72c880d1cef201ca3034d2d7
Enzo Eghermanne <enzo.eghermanne@gmail.com>
2019-06-12 11:38:43 +0200

    feat: updated story for Checkbox

--- src/components/Checkbox/Checkbox.stories.tsx
+++ src/components/Checkbox/Checkbox.stories.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
 import { storiesOf } from '@storybook/react';
 
@@ -8,9 +8,27 @@ import { Checkbox } from './Checkbox';
 
 storiesOf('Checkbox', module)
 	.addParameters({ jest: ['Checkbox'] })
-	.add('Checkbox', () => <Checkbox label="One" onChange={action('Checkbox toggled')} />)
+	.add('Checkbox', () => (
+		<Fragment>
+			<Checkbox label="Sodium" onChange={action('Checkbox toggled')} />
+			<br />
+			<Checkbox label="Hydrogen" onChange={action('Checkbox toggled')} />
+			<br />
+			<Checkbox label="Oxygen" onChange={action('Checkbox toggled')} />
+			<br />
+			<Checkbox label="Nitrogen" onChange={action('Checkbox toggled')} />
+		</Fragment>
+	))
 	.add('Checkbox default checked', () => (
-		<Checkbox label="One" defaultChecked={true} onChange={action('Checkbox toggled')} />
+		<Fragment>
+			<Checkbox label="Sodium" defaultChecked={true} onChange={action('Checkbox toggled')} />
+			<br />
+			<Checkbox label="Hydrogen" defaultChecked={true} onChange={action('Checkbox toggled')} />
+			<br />
+			<Checkbox label="Oxygen" defaultChecked={true} onChange={action('Checkbox toggled')} />
+			<br />
+			<Checkbox label="Nitrogen" defaultChecked={true} onChange={action('Checkbox toggled')} />
+		</Fragment>
 	))
 	.add('Checkbox default unchecked', () => (
 		<Checkbox label="One" defaultChecked={false} onChange={action('Checkbox toggled')} />
