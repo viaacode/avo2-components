6f43b869b46958b3204c2eef7dd29943f8fa1240
Benjamin Naesen <benjamin.naesen@studiohyperdrive.be>
2019-06-12 18:03:48 +0200

    feat: added loaded state for multicolor

--- src/components/Thumbnail/Thumbnail.stories.tsx
+++ src/components/Thumbnail/Thumbnail.stories.tsx
@@ -8,26 +8,31 @@ storiesOf('Thumbnail', module)
 	.addParameters({ jest: ['Thumbnail'] })
 	.add('Thumbnail (image)', () => (
 		<div className="o-grid-col-bp3-4">
-			<Thumbnail src="images/thumbnail.jpg" href="#" count="4" icon="video" label="collectie" />
+			<Thumbnail src="images/thumbnail.jpg" meta="4 items" category="video" label="collectie" />
 		</div>
 	))
 	.add('Thumbnail (placeholder)', () => (
 		<div className="o-grid-col-bp3-4">
-			<Thumbnail href="#" count="4" icon="video" label="collectie" />
+			<Thumbnail meta="4 items" category="collection" label="collectie" />
 		</div>
 	))
 	.add('Thumbnail with label', () => (
 		<div className="o-grid-col-bp3-4">
-			<Thumbnail src="images/thumbnail.jpg" href="#" count="4" icon="video" label="collectie" />
+			<Thumbnail
+				src="images/thumbnail.jpg"
+				category="collection"
+				meta="4 items"
+				label="collectie"
+			/>
 		</div>
 	))
 	.add('Thumbnail without label', () => (
 		<div className="o-grid-col-bp3-4">
-			<Thumbnail src="images/thumbnail.jpg" href="#" count="4" icon="video" />
+			<Thumbnail src="images/thumbnail.jpg" category="video" meta="2:22" />
 		</div>
 	))
-	.add('Thumbnail without count', () => (
+	.add('Thumbnail without meta', () => (
 		<div className="o-grid-col-bp3-4">
-			<Thumbnail src="images/thumbnail.jpg" href="#" icon="video" label="collectie" />
+			<Thumbnail src="images/thumbnail.jpg" category="video" label="collectie" />
 		</div>
 	));
--- src/components/Thumbnail/Thumbnail.tsx
+++ src/components/Thumbnail/Thumbnail.tsx
@@ -3,38 +3,56 @@ import React, { FunctionComponent } from 'react';
 import { Icon } from '../Icon/Icon';
 
 export interface ThumbnailProps {
+	category: 'collection' | 'video' | 'audio';
 	src?: string;
-	icon: string;
+	alt?: string;
 	label?: string;
-	count?: string;
-	href: string;
+	meta?: string;
 }
 
-export const Thumbnail: FunctionComponent<ThumbnailProps> = ({
-	src,
-	icon,
-	label,
-	count,
-	href,
-}: ThumbnailProps) => {
-	return (
-		<div className="c-media-card c-media-card--collection">
-			<a className="c-media-card-thumb" href={href}>
-				<div className="c-thumbnail c-thumbnail-media c-thumbnail-media--collection">
-					{src ? (
-						<img src={src} className="c-thumbnail-placeholder" />
-					) : (
-						<div className="c-thumbnail-placeholder">{icon && <Icon name={icon} />}</div>
-					)}
-					<div className="c-thumbnail-meta">
-						<div className="c-thumbnail-media__category">
-							<Icon name={icon} />
-							{label && <p>{label}</p>}
+export interface ThumbnailState {
+	loaded: boolean;
+}
+
+export class Thumbnail extends React.Component<ThumbnailProps, ThumbnailState> {
+	constructor(props: ThumbnailProps) {
+		super(props);
+
+		this.state = {
+			loaded: false,
+		};
+	}
+
+	handleImageLoaded() {
+		this.setState({ loaded: true });
+	}
+
+	render() {
+		const { category, src, alt, label, meta }: ThumbnailProps = this.props;
+		const { loaded }: ThumbnailState = this.state;
+
+		const metaClass = loaded ? 'c-thumbnail-meta--img-is-loaded' : '';
+
+		return (
+			<div className="c-media-card c-media-card--video c-media-card--horizontal">
+				<a className="c-media-card-thumb" href="/app/single/item.html">
+					<div className={`c-thumbnail c-thumbnail-media c-thumbnail-media--${category}`}>
+						<div className="c-thumbnail-placeholder">{category && <Icon name={category} />}</div>
+						{src && (
+							<div className="c-thumbnail-image">
+								<img src={src} alt={alt} onLoad={this.handleImageLoaded.bind(this)} />
+							</div>
+						)}
+						<div className={`c-thumbnail-meta ${metaClass}`}>
+							<div className="c-thumbnail-media__category">
+								<Icon name={category} />
+								{label && <p>{label}</p>}
+							</div>
+							{meta && <div className="c-thumbnail-media__meta">{meta}</div>}
 						</div>
-						{count && <div className="c-thumbnail-media__meta">{count} items</div>}
 					</div>
-				</div>
-			</a>
-		</div>
-	);
-};
+				</a>
+			</div>
+		);
+	}
+}
