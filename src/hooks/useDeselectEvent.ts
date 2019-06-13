11992bbf0a78c260c48a465d744a4b2bb7991e18
Enzo Eghermanne <enzo.eghermanne@gmail.com>
2019-06-13 14:40:33 +0200

    fix: added support for numeric values as name/value

--- src/hooks/useDeselectEvent.ts
+++ src/hooks/useDeselectEvent.ts
@@ -4,7 +4,7 @@ import { useEffect } from 'react';
 // with a shared `name`-attribute to ensure that their state is kept in sync (except the selected radiobutton)
 export function useDeselectEvent(name: string, value: string, onDeselect: () => void) {
 	useEffect(() => {
-		const element = document.querySelector(`input[type="radio"][name=${name}][value=${value}]`);
+		const element = document.querySelector(`input[type="radio"][name="${name}"][value="${value}"]`);
 
 		if (element) {
 			element.addEventListener('deselect', onDeselect);
@@ -19,10 +19,8 @@ export function useDeselectEvent(name: string, value: string, onDeselect: () => 
 	return [
 		() => {
 			document
-				.querySelectorAll(`input[type="radio"][name=${name}]:not([value=${value}])`)
-				.forEach((element: Element) => {
-					element.dispatchEvent(new Event('deselect'));
-				});
+				.querySelectorAll(`input[type="radio"][name="${name}"]:not([value="${value}"])`)
+				.forEach((element: Element) => element.dispatchEvent(new Event('deselect')));
 		},
 	];
 }
