0d0125a565e01408ab1692f7919da46fff3a9a60
Enzo Eghermanne <enzo.eghermanne@gmail.com>
2019-06-13 08:17:09 +0200

    feat: added storybook-action to ToggleButton

--- src/components/ToggleButton/ToggleButton.stories.tsx
+++ src/components/ToggleButton/ToggleButton.stories.tsx
@@ -2,9 +2,15 @@ import React from 'react';
 
 import { storiesOf } from '@storybook/react';
 
+import { action } from '../../helpers/action';
+
 import { ToggleButton } from './ToggleButton';
 
 storiesOf('ToggleButton', module)
 	.addParameters({ jest: ['ToggleButton'] })
-	.add('ToggleButton', () => <ToggleButton icon="heart" active={false} />)
-	.add('ToggleButton active', () => <ToggleButton icon="bookmark" active={true} />);
+	.add('ToggleButton', () => (
+		<ToggleButton icon="heart" active={false} onClick={action('onClick')} />
+	))
+	.add('ToggleButton active', () => (
+		<ToggleButton icon="bookmark" active={true} onClick={action('onClick')} />
+	));
