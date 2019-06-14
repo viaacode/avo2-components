9bc51b5a59181b66630663faca8ba9fa86998d53
Bert Verhelst <verhelstbert@gmail.com>
2019-06-14 11:22:56 +0200

    fix: change ToolbarItem element from h2 to div

--- src/components/Toolbar/ToolbarItem.tsx
+++ src/components/Toolbar/ToolbarItem.tsx
@@ -7,5 +7,5 @@ export interface ToolbarItemProps {
 export const ToolbarItem: FunctionComponent<ToolbarItemProps> = ({
 	children,
 }: ToolbarItemProps) => {
-	return <h2 className="c-toolbar__item">{children}</h2>;
+	return <div className="c-toolbar__item">{children}</div>;
 };
