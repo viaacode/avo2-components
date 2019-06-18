2a2d4abf5bf67e20f7f01dd80b3636cccb3e08f8
Enzo Eghermanne <enzo.eghermanne@gmail.com>
2019-06-18 13:54:18 +0200

    feat: render className conditionally based on swatches or not

--- src/components/TagList/TagList.tsx
+++ src/components/TagList/TagList.tsx
@@ -28,7 +28,9 @@ export const TagList: FunctionComponent<TagListProps> = ({
 							className={classNames('c-label-swatch', `c-label-swatch--color-${(index % 10) + 1}`)}
 						/>
 					)}
-					<p className="c-tag__label">{tag}</p>
+					<p className={classNames({ 'c-tag__label': !swatches, 'c-label-text': swatches })}>
+						{tag}
+					</p>
 					{closable && (
 						<a onClick={() => onTagClosed(tag)}>
 							<Icon name="close" />
