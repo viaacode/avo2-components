ed4fbfef1bc69d3890500c531b529b3c2129ad58
Benjamin Naesen <benjamin.naesen@studiohyperdrive.be>
2019-06-18 11:52:15 +0200

    refactor: convert to functional component

--- src/components/Thumbnail/Thumbnail.tsx
+++ src/components/Thumbnail/Thumbnail.tsx
@@ -1,4 +1,6 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useState } from 'react';
+
+import classNames from 'classnames';
 
 import { Icon } from '../Icon/Icon';
 
@@ -14,41 +16,34 @@ export interface ThumbnailState {
 	loaded: boolean;
 }
 
-export class Thumbnail extends React.Component<ThumbnailProps, ThumbnailState> {
-	constructor(props: ThumbnailProps) {
-		super(props);
+export const Thumbnail: FunctionComponent<ThumbnailProps> = ({
+	category,
+	src,
+	alt,
+	label,
+	meta,
+}: ThumbnailProps) => {
+	const [loaded, setLoaded] = useState(false);
 
-		this.state = {
-			loaded: false,
-		};
-	}
-
-	handleImageLoaded() {
-		this.setState({ loaded: true });
-	}
-
-	render() {
-		const { category, src, alt, label, meta }: ThumbnailProps = this.props;
-		const { loaded }: ThumbnailState = this.state;
-
-		const metaClass = loaded ? 'c-thumbnail-meta--img-is-loaded' : '';
-
-		return (
-			<div className={`c-thumbnail c-thumbnail-media c-thumbnail-media--${category}`}>
-				<div className="c-thumbnail-placeholder">{category && <Icon name={category} />}</div>
-				{src && (
-					<div className="c-thumbnail-image">
-						<img src={src} alt={alt} onLoad={this.handleImageLoaded.bind(this)} />
-					</div>
-				)}
-				<div className={`c-thumbnail-meta ${metaClass}`}>
-					<div className="c-thumbnail-media__category">
-						<Icon name={category} />
-						{label && <p>{label}</p>}
-					</div>
-					{meta && <div className="c-thumbnail-media__meta">{meta}</div>}
+	return (
+		<div className={`c-thumbnail c-thumbnail-media c-thumbnail-media--${category}`}>
+			<div className="c-thumbnail-placeholder">{category && <Icon name={category} />}</div>
+			{src && (
+				<div className="c-thumbnail-image">
+					<img src={src} alt={alt} onLoad={() => setLoaded(true)} />
 				</div>
+			)}
+			<div
+				className={classNames('c-thumbnail-meta', {
+					'c-thumbnail-meta--img-is-loaded': loaded,
+				})}
+			>
+				<div className="c-thumbnail-media__category">
+					<Icon name={category} />
+					{label && <p>{label}</p>}
+				</div>
+				{meta && <div className="c-thumbnail-media__meta">{meta}</div>}
 			</div>
-		);
-	}
-}
+		</div>
+	);
+};
