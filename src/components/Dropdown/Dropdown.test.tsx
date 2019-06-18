dea16c90c47b5c57cfb11e6d5e07f5832b6fbd17
Enzo Eghermanne <enzo.eghermanne@gmail.com>
2019-06-18 10:06:16 +0200

    test: updated <Dropdown> tests

--- src/components/Dropdown/Dropdown.test.tsx
+++ src/components/Dropdown/Dropdown.test.tsx
@@ -7,7 +7,7 @@ import { Dropdown } from './Dropdown';
 describe('<Dropdown />', () => {
 	it('Should be able to render', () => {
 		shallow(
-			<Dropdown label="Show options">
+			<Dropdown label="Show options" isOpen={false}>
 				<div>OneOneOneOneOneOne</div>
 				<div>Two</div>
 				<div>Three</div>
@@ -17,9 +17,9 @@ describe('<Dropdown />', () => {
 		);
 	});
 
-	it('Should by default render with a closed flyout', () => {
+	it('Should render correctly with `isOpen = false`', () => {
 		const dropdownComponent = shallow(
-			<Dropdown label="Show options">
+			<Dropdown label="Show options" isOpen={false}>
 				<div>OneOneOneOneOneOne</div>
 				<div>Two</div>
 				<div>Three</div>
@@ -31,9 +31,26 @@ describe('<Dropdown />', () => {
 		expect(dropdownComponent.find('.c-menu')).toHaveLength(0);
 	});
 
-	it('Should open flyout when clicking the button', () => {
+	it('Shouldrender correctly with `isOpen = true`', () => {
 		const dropdownComponent = mount(
-			<Dropdown label="Show options">
+			<Dropdown label="Show options" isOpen={true}>
+				<div>OneOneOneOneOneOne</div>
+				<div>Two</div>
+				<div>Three</div>
+				<div>Four</div>
+				<div>Five</div>
+			</Dropdown>
+		);
+
+		expect(dropdownComponent.find('.c-menu')).toHaveLength(1);
+		expect(dropdownComponent.find('.c-menu--visible')).toHaveLength(1);
+	});
+
+	it('Should call `onOpen` when clicking the button (and `isOpen = false`)', () => {
+		const onOpenHandler = jest.fn();
+
+		const dropdownComponent = mount(
+			<Dropdown label="Show options" isOpen={false} onOpen={onOpenHandler}>
 				<div>OneOneOneOneOneOne</div>
 				<div>Two</div>
 				<div>Three</div>
@@ -47,13 +64,35 @@ describe('<Dropdown />', () => {
 			.first()
 			.simulate('click');
 
-		expect(dropdownComponent.find('.c-menu')).toHaveLength(1);
-		expect(dropdownComponent.find('.c-menu--visible')).toHaveLength(1);
+		expect(onOpenHandler).toHaveBeenCalled();
+		expect(onOpenHandler).toHaveBeenCalledTimes(1);
+	});
+
+	it('Should call `onClose` when clicking the button (and `isOpen = true`)', () => {
+		const onCloseHandler = jest.fn();
+
+		const dropdownComponent = mount(
+			<Dropdown label="Show options" isOpen={true} onClose={onCloseHandler}>
+				<div>OneOneOneOneOneOne</div>
+				<div>Two</div>
+				<div>Three</div>
+				<div>Four</div>
+				<div>Five</div>
+			</Dropdown>
+		);
+
+		dropdownComponent
+			.find('button')
+			.first()
+			.simulate('click');
+
+		expect(onCloseHandler).toHaveBeenCalled();
+		expect(onCloseHandler).toHaveBeenCalledTimes(1);
 	});
 
 	it('Should set the correct className for button', () => {
 		const dropdownComponent = mount(
-			<Dropdown label="Show options">
+			<Dropdown label="Show options" isOpen={false}>
 				<div>OneOneOneOneOneOne</div>
 				<div>Two</div>
 				<div>Three</div>
@@ -74,7 +113,7 @@ describe('<Dropdown />', () => {
 		const label = 'Test label';
 
 		const dropdownComponent = mount(
-			<Dropdown label={label}>
+			<Dropdown label={label} isOpen={false}>
 				<div>OneOneOneOneOneOne</div>
 				<div>Two</div>
 				<div>Three</div>
