17211950fd0dc30ba76c271ece0d424ccb94a9b2
Benjamin Naesen <benjamin.naesen@studiohyperdrive.be>
2019-07-04 10:44:32 +0200

    refactor: dummy content in variable

--- src/components/Flex/Flex.stories.tsx
+++ src/components/Flex/Flex.stories.tsx
@@ -10,68 +10,30 @@ const content1 = loremIpsum({ count: 1 });
 const content2 = loremIpsum({ count: 4 });
 const content3 = loremIpsum({ count: 2 });
 
+const dummyFlexItems = (
+	<Fragment>
+		<FlexItem>{content1}</FlexItem>
+		<FlexItem>{content2}</FlexItem>
+		<FlexItem>{content3}</FlexItem>
+	</Fragment>
+);
+
 storiesOf('Flex', module)
 	.addParameters({ jest: ['Flex'] })
-	.add('Flex', () => (
-		<Flex>
-			<FlexItem>{content1}</FlexItem>
-			<FlexItem>{content2}</FlexItem>
-			<FlexItem>{content3}</FlexItem>
-		</Flex>
-	))
-	.add('Flex (horizontal)', () => (
-		<Flex orientation="horizontal">
-			<FlexItem>{content1}</FlexItem>
-			<FlexItem>{content2}</FlexItem>
-			<FlexItem>{content3}</FlexItem>
-		</Flex>
-	))
-	.add('Flex (vertical)', () => (
-		<Flex orientation="vertical">
-			<FlexItem>{content1}</FlexItem>
-			<FlexItem>{content2}</FlexItem>
-			<FlexItem>{content3}</FlexItem>
-		</Flex>
-	))
-	.add('Flex (center)', () => (
-		<Flex center>
-			<FlexItem>{content1}</FlexItem>
-			<FlexItem>{content2}</FlexItem>
-			<FlexItem>{content3}</FlexItem>
-		</Flex>
-	))
+	.add('Flex', () => <Flex>{dummyFlexItems}</Flex>)
+	.add('Flex (horizontal)', () => <Flex orientation="horizontal">{dummyFlexItems}</Flex>)
+	.add('Flex (vertical)', () => <Flex orientation="vertical">{dummyFlexItems}</Flex>)
+	.add('Flex (center)', () => <Flex center>{dummyFlexItems}</Flex>)
 	.add('Flex (vertical - center)', () => (
 		<Flex orientation="vertical" center>
-			<FlexItem>{content1}</FlexItem>
-			<FlexItem>{content2}</FlexItem>
-			<FlexItem>{content3}</FlexItem>
+			{dummyFlexItems}
 		</Flex>
 	))
 	.add('Flex (horizontal - center)', () => (
 		<Flex orientation="horizontal" center>
-			<FlexItem>{content1}</FlexItem>
-			<FlexItem>{content2}</FlexItem>
-			<FlexItem>{content3}</FlexItem>
+			{dummyFlexItems}
 		</Flex>
 	))
-	.add('Flex (justify)', () => (
-		<Flex justify>
-			<FlexItem>{content1}</FlexItem>
-			<FlexItem>{content2}</FlexItem>
-			<FlexItem>{content3}</FlexItem>
-		</Flex>
-	))
-	.add('Flex (spaced - regular)', () => (
-		<Flex spaced="regular">
-			<FlexItem>{content1}</FlexItem>
-			<FlexItem>{content2}</FlexItem>
-			<FlexItem>{content3}</FlexItem>
-		</Flex>
-	))
-	.add('Flex (spaced - wide)', () => (
-		<Flex spaced="wide">
-			<FlexItem>{content1}</FlexItem>
-			<FlexItem>{content2}</FlexItem>
-			<FlexItem>{content3}</FlexItem>
-		</Flex>
-	));
+	.add('Flex (justify)', () => <Flex justify>{dummyFlexItems}</Flex>)
+	.add('Flex (spaced - regular)', () => <Flex spaced="regular">{dummyFlexItems}</Flex>)
+	.add('Flex (spaced - wide)', () => <Flex spaced="wide">{dummyFlexItems}</Flex>);
