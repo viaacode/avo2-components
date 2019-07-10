96ec3a7a8c6c796787c47077be11b45452793fa3
Enzo Eghermanne <enzo.eghermanne@gmail.com>
2019-07-10 11:55:20 +0200

    feat: kickstart <WYSIWYG />

---
+++ src/components/WYSIWYG/WYSIWYG.stories.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import { storiesOf } from '@storybook/react';
+
+import { WYSIWYG } from './WYSIWYG';
+
+const withContent = (story: Function) => <div className="c-content">{story()}</div>;
+
+storiesOf('WYSIWYG', module)
+	.addParameters({ jest: ['WYSIWYG'] })
+	.addDecorator(withContent)
+	.add('WYSIWYG', () => (
+		<WYSIWYG
+			id="story-wysiwyg-1"
+			data={'<h2>Welcome!</h2><p>This prefilled content is all <strong>editable</strong>.</p>'}
+		/>
+	))
+	.add('WYSIWYG with limited buttons', () => (
+		<WYSIWYG
+			id="story-wysiwyg-2"
+			buttons={[
+				['strong', 'em'],
+				['undo', 'redo'],
+				['formatting'],
+				['unorderedList', 'orderedList'],
+				['removeformat'],
+				['link'],
+			]}
+		/>
+	));
--- src/index.ts
+++ src/index.ts
@@ -79,6 +79,7 @@ import { Toolbar } from './components/Toolbar/Toolbar';
 import { ToolbarCenter, ToolbarLeft, ToolbarRight } from './components/Toolbar/Toolbar.slots';
 import { ToolbarItem } from './components/Toolbar/ToolbarItem';
 import { ToolbarTitle } from './components/Toolbar/ToolbarTitle';
+import { WYSIWYG } from './components/WYSIWYG/WYSIWYG';
 
 export {
 	COLORS,
@@ -156,4 +157,5 @@ export {
 	ToolbarRight,
 	ToolbarItem,
 	ToolbarTitle,
+	WYSIWYG,
 };
