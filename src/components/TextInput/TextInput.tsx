dc9584fd93560a9abdd79de584d77aca5a0d0fbf
Benjamin Naesen <benjamin.naesen@studiohyperdrive.be>
2019-07-11 15:20:18 +0200

    feat: add email type

--- src/components/TextInput/TextInput.tsx
+++ src/components/TextInput/TextInput.tsx
@@ -2,7 +2,7 @@ import React, { ChangeEvent, FunctionComponent } from 'react';
 
 import { Icon } from '../Icon/Icon';
 
-type Type = 'password' | 'text';
+type Type = 'password' | 'text' | 'email';
 
 export interface TextInputProps {
 	id?: string;
