bb8f80579094711f885d0c5818c5b22056de6c95
Andry Charlier <andry.charlier@studiohyperdrive.be>
2019-07-19 10:38:16 +0200

    feat(alert): update stories with dark mode

--- src/components/Alert/Alert.stories.tsx
+++ src/components/Alert/Alert.stories.tsx
@@ -6,10 +6,34 @@ import { Alert } from './Alert';
 storiesOf('Alert', module)
 	.addParameters({ jest: ['Alert'] })
 	.add('Alerts', () => (
-		<Alert
-			id="1"
-			message="Alert message"
-			options={{ type: 'info', dark: false }}
-			close={() => {}}
-		/>
+		<React.Fragment>
+			<div className="u-spacer-bottom">
+				<Alert message="Info alert message" close={() => {}} />
+			</div>
+			<div className="u-spacer-bottom">
+				<Alert message="Success alert message" type="success" close={() => {}} />
+			</div>
+			<div className="u-spacer-bottom">
+				<Alert message="Danger alert message" type="danger" close={() => {}} />
+			</div>
+			<div className="u-spacer-bottom">
+				<Alert message="Loading alert message" type="spinner" close={() => {}} />
+			</div>
+		</React.Fragment>
+	))
+	.add('Dark alerts', () => (
+		<React.Fragment>
+			<div className="u-spacer-bottom">
+				<Alert message="Info alert message" dark={true} close={() => {}} />
+			</div>
+			<div className="u-spacer-bottom">
+				<Alert message="Success alert message" type="success" dark={true} close={() => {}} />
+			</div>
+			<div className="u-spacer-bottom">
+				<Alert message="Danger alert message" type="danger" dark={true} close={() => {}} />
+			</div>
+			<div className="u-spacer-bottom">
+				<Alert message="Loading alert message" type="spinner" dark={true} close={() => {}} />
+			</div>
+		</React.Fragment>
 	));
