bfffd25b47c745e0a8f2176b59aa2dec1d0e132c
Benjamin Naesen <benjamin.naesen@studiohyperdrive.be>
2019-09-02 10:32:54 +0200

    refactor: remove redundant story

--- src/components/Toggle/Toggle.stories.tsx
+++ src/components/Toggle/Toggle.stories.tsx
@@ -35,9 +35,4 @@ storiesOf('Toggle', module)
 		<ToggleStoryComponent defaultChecked={true}>
 			<Toggle />
 		</ToggleStoryComponent>
-	))
-	.add('Toggle default unchecked', () => (
-		<ToggleStoryComponent defaultChecked={false}>
-			<Toggle />
-		</ToggleStoryComponent>
 	));
