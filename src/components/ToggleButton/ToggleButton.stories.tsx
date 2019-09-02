0e3b432e2860ca15b9459085b26076e3618f3b59
Bert Verhelst <verhelstbert@gmail.com>
2019-09-02 10:20:09 +0200

    fix: add examples of a tooltip in the stories of button and toggle btn

--- src/components/ToggleButton/ToggleButton.stories.tsx
+++ src/components/ToggleButton/ToggleButton.stories.tsx
@@ -14,6 +14,7 @@ storiesOf('ToggleButton', module)
 			active={false}
 			onClick={action('onClick')}
 			ariaLabel="toggle favorite"
+			title="toggle favorite"
 		/>
 	))
 	.add('ToggleButton active', () => (
