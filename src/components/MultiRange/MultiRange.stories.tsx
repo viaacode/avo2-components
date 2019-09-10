d07284e3e2a4025b8c260c47d5854f021f3f89fa
Bert Verhelst <verhelstbert@gmail.com>
2019-09-10 17:27:11 +0200

    feat: add multirange component

---
+++ src/components/MultiRange/MultiRange.stories.tsx
@@ -0,0 +1,48 @@
+import React, { cloneElement, Fragment, ReactElement, useState } from 'react';
+
+import { storiesOf } from '@storybook/react';
+
+import { action } from '../../helpers/action';
+
+import { MultiRange } from './MultiRange';
+
+const MultiRangeStoryComponent = ({
+	children,
+	defaultValues = [0, 100],
+}: {
+	children: ReactElement;
+	defaultValues?: number[];
+}) => {
+	const [values, setValues] = useState(defaultValues);
+
+	return cloneElement(children, {
+		values,
+		onChange: (values: number[]) => {
+			action('Input changed')(values);
+			setValues(values);
+		},
+	});
+};
+
+storiesOf('MultiRange', module)
+	.addParameters({ jest: ['MultiRange'] })
+	.add('MultiRange', () => (
+		<MultiRangeStoryComponent>
+			<MultiRange />
+		</MultiRangeStoryComponent>
+	))
+	.add('MultiRange disabled', () => (
+		<MultiRangeStoryComponent>
+			<MultiRange disabled />
+		</MultiRangeStoryComponent>
+	))
+	.add('MultiRange with default values', () => (
+		<MultiRangeStoryComponent defaultValues={[20, 60]}>
+			<MultiRange />
+		</MultiRangeStoryComponent>
+	))
+	.add('MultiRange with single value', () => (
+		<MultiRangeStoryComponent defaultValues={[20]}>
+			<MultiRange />
+		</MultiRangeStoryComponent>
+	));
