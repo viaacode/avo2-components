9999487b9e31d5de724739ad73b2d537c1b91702
Bert Verhelst <verhelstbert@gmail.com>
2019-09-18 11:27:22 +0200

    fix: default to slash icon if name prop isn't passed

--- src/components/Icon/Icon.tsx
+++ src/components/Icon/Icon.tsx
@@ -6,14 +6,20 @@ import { DefaultProps } from '../../types';
 import * as Icons from '../Icons';
 
 export interface IconProps extends DefaultProps {
-	name: string;
+	name?: string;
 	size?: 'small' | 'large' | 'huge';
 	type?: 'arrows' | 'custom' | 'multicolor' | 'social' | 'wysiwyg';
 	active?: boolean;
 }
 
-export const Icon: FunctionComponent<IconProps> = ({ className, name, size, type, active }) => {
-	let IconToRender = (Icons as any)[toPascalCase(name || 'slash')];
+export const Icon: FunctionComponent<IconProps> = ({
+	className,
+	name = 'slash',
+	size,
+	type,
+	active,
+}) => {
+	let IconToRender = (Icons as any)[toPascalCase(name)];
 
 	function getIconName() {
 		const base = 'o-svg-icon';
