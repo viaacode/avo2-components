59afcd888d7dffbba80fac3a9512ec311507846b
Bert Verhelst <verhelstbert@gmail.com>
2019-09-20 14:51:24 +0200

    fix: remove unused import

--- src/components/WYSIWYG/WYSIWYG.tsx
+++ src/components/WYSIWYG/WYSIWYG.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent, useState } from 'react';
+import React, { FunctionComponent } from 'react';
 
 import Trumbowyg from 'react-trumbowyg';
 import 'trumbowyg/dist/plugins/table/trumbowyg.table';
