cbd0983a17ac4bfe57fd66374bc6d752d23f5a31
Bert Verhelst <verhelstbert@gmail.com>
2019-09-20 14:23:45 +0200

    fix: wysiwyg caret jumpt to front

--- src/components/WYSIWYG/WYSIWYG.tsx
+++ src/components/WYSIWYG/WYSIWYG.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useState } from 'react';
 
 import Trumbowyg from 'react-trumbowyg';
 import 'trumbowyg/dist/plugins/table/trumbowyg.table';
@@ -26,7 +26,7 @@ export interface WYSIWYGProps {
 	onFocus?: TrumbowygEvent;
 	onBlur?: TrumbowygEvent;
 	onInit?: TrumbowygEvent;
-	onChange?: TrumbowygEvent;
+	onChange?: (html: string) => void;
 	onResize?: TrumbowygEvent;
 	onPaste?: TrumbowygEvent;
 	onOpenFullScreen?: TrumbowygEvent;
@@ -59,31 +59,46 @@ export const WYSIWYG: FunctionComponent<WYSIWYGProps> = ({
 	onOpenFullScreen = () => {},
 	onCloseFullScreen = () => {},
 	onClose = () => {},
-}: WYSIWYGProps) => (
-	<Trumbowyg
-		id={id}
-		data={data}
-		placeholder={placeholder}
-		lang={lang}
-		buttons={buttons}
-		semantic={semantic}
-		resetCss={resetCss}
-		autogrow={autogrow}
-		disabled={disabled}
-		removeformatPasted={removeformatPasted}
-		shouldUseSvgIcons={shouldUseSvgIcons}
-		shouldInjectSvgIcons={shouldInjectSvgIcons}
-		svgIconsPath={svgIconsPath}
-		btnsDef={btnsDef}
-		plugins={plugins}
-		onFocus={onFocus}
-		onBlur={onBlur}
-		onInit={onInit}
-		onChange={onChange}
-		onResize={onResize}
-		onPaste={onPaste}
-		onOpenFullScreen={onOpenFullScreen}
-		onCloseFullScreen={onCloseFullScreen}
-		onClose={onClose}
-	/>
-);
+}: WYSIWYGProps) => {
+	let html: string;
+
+	const handleBlur = (event: JQuery.Event) => {
+		if (html) {
+			onChange(html);
+		}
+		onBlur(event);
+	};
+
+	const handleChange = (event: any) => {
+		html = event.target.innerHTML;
+	};
+
+	return (
+		<Trumbowyg
+			id={id}
+			data={data}
+			placeholder={placeholder}
+			lang={lang}
+			buttons={buttons}
+			semantic={semantic}
+			resetCss={resetCss}
+			autogrow={autogrow}
+			disabled={disabled}
+			removeformatPasted={removeformatPasted}
+			shouldUseSvgIcons={shouldUseSvgIcons}
+			shouldInjectSvgIcons={shouldInjectSvgIcons}
+			svgIconsPath={svgIconsPath}
+			btnsDef={btnsDef}
+			plugins={plugins}
+			onFocus={onFocus}
+			onBlur={handleBlur}
+			onInit={onInit}
+			onChange={handleChange}
+			onResize={onResize}
+			onPaste={onPaste}
+			onOpenFullScreen={onOpenFullScreen}
+			onCloseFullScreen={onCloseFullScreen}
+			onClose={onClose}
+		/>
+	);
+};
