effd42790444e02aa4cefbe0f987460b4382ab3c
Bert Verhelst <verhelstbert@gmail.com>
2019-09-23 15:53:09 +0200

    fix: add active class when active option is set to true for a button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -46,7 +46,9 @@ const Button: FunctionComponent<ButtonProps> = ({
 	<button
 		className={classNames(className, 'c-button', {
 			active,
+			// Not sure why there are 3 classes in mono styles: c-button-action, c-button--active, active
 			'c-button-action': active,
+			'c-button--active': active,
 			'c-button--small': size === 'small',
 			'c-button--block': block,
 			'c-button--icon': icon && !label,
