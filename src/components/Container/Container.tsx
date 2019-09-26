a45608da27e65ee843e63b667874a29d275c37ba
Andry Charlier <andry.charlier@studiohyperdrive.be>
2019-09-26 15:56:25 +0200

    fix: remove unused classNames

--- src/components/Container/Container.tsx
+++ src/components/Container/Container.tsx
@@ -27,7 +27,6 @@ export const Container: FunctionComponent<ContainerProps> = ({
 			'o-container-vertical': mode === 'vertical',
 			[`o-container--${size}`]: (mode === 'horizontal' || !mode) && size,
 			[`o-container-vertical--${size}`]: mode === 'vertical' && size,
-			[`o-container-vertical--padding-${size}`]: mode === 'vertical' && size,
 			[`o-container-vertical--bg-${background}`]: background,
 			'o-container-vertical--bottom-bordered': bordered,
 		})}
