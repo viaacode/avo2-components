c1a1eeec058831b6bb3f14efae977d17d59044e5
Benjamin Naesen <benjamin@naesen.be>
2019-10-09 11:07:49 +0200

    fix: title styling

--- src/components/FlowPlayer/FlowPlayer.mock.tsx
+++ src/components/FlowPlayer/FlowPlayer.mock.tsx
@@ -1,7 +1,7 @@
 import { FlowPlayerProps } from './FlowPlayer';
 
 export const MOCK_FLOW_PLAYER_PROPS: FlowPlayerProps = {
-	src: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4',
+	src: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4',
 	poster: 'https://via.placeholder.com/1920x1080',
 	title: 'Title',
 	subtitles: ['30-12-2011', 'VRT'],
