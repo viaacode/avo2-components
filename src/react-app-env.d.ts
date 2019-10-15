25f2c244980429aa92831d9ca96deb25971cd2e2
Andry Charlier <andry.charlier@studiohyperdrive.be>
2019-10-15 14:40:51 +0200

    fix: declare flowplayer-files plugins for ts

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -3,3 +3,6 @@
 declare module 'react-trumbowyg';
 declare module 'trumbowyg/dist/plugins/table/trumbowyg.table';
 declare module 'flowplayer-files';
+declare module 'flowplayer-files/lib/plugins/chromecast.min';
+declare module 'flowplayer-files/lib/plugins/cuepoints.min';
+declare module 'flowplayer-files/lib/plugins/subtitles.min';
