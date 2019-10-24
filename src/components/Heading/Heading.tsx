b854932e046a090c3f2d8b5af10e5a4a7c4bed5a
Benjamin Naesen <benjamin@naesen.be>
2019-10-24 15:13:51 +0200

    refactor(heading): destructure props into tag

--- src/components/Heading/Heading.tsx
+++ src/components/Heading/Heading.tsx
@@ -4,13 +4,11 @@ import classNames from 'classnames';
 
 import { DefaultProps } from '../../types';
 
-import './Heading.scss';
-
 export interface HeadingProps extends DefaultProps {
 	type: 'h1' | 'h2' | 'h3' | 'h4';
-	children: 'string' | ReactNode;
+	children: ReactNode;
 }
 
-export const Heading: FunctionComponent<HeadingProps> = ({ className, children, ...props }) => (
-	<props.type className={classNames(className, `c-${props.type}`)}>{children}</props.type>
+export const Heading: FunctionComponent<HeadingProps> = ({ className, children, type: Type }) => (
+	<Type className={classNames(className, `c-${Type}`)}>{children}</Type>
 );
