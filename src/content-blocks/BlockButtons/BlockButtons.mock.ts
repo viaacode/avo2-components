5b9f7944873ab4a4dbbbb2183262c5fbcd3e3b5e
Benjamin Naesen <benjamin@naesen.be>
2020-01-08 15:10:52 +0100

    feat: add actions for BlockButtons

--- src/content-blocks/BlockButtons/BlockButtons.mock.ts
+++ src/content-blocks/BlockButtons/BlockButtons.mock.ts
@@ -1,1 +1,12 @@
-export const BUTTONS_MOCK = [{ label: 'Knop 1' }, { label: 'Knop 2' }];
+import { action } from '../../helpers/action';
+
+export const BUTTONS_MOCK = [
+	{
+		label: 'Knop 1',
+		onClick: action('Button 1 clicked'),
+	},
+	{
+		label: 'Knop 2',
+		onClick: action('Button 2 clicked'),
+	},
+];
