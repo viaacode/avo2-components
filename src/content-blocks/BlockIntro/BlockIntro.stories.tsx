7eff15d8cab870a2de21abd9df686fdf3e9e0c9e
Benjamin Naesen <benjamin@naesen.be>
2020-01-23 14:57:15 +0100

    feat(cb): add align story for intro

--- src/content-blocks/BlockIntro/BlockIntro.stories.tsx
+++ src/content-blocks/BlockIntro/BlockIntro.stories.tsx
@@ -7,4 +7,7 @@ import { BlockIntro } from './BlockIntro';
 
 storiesOf('BlockIntro', module)
 	.addParameters({ jest: ['BlockIntro'] })
-	.add('BlockIntro', () => <BlockIntro title="Page title" content={loremIpsum({ count: 3 })} />);
+	.add('BlockIntro', () => <BlockIntro title="Page title" content={loremIpsum({ count: 3 })} />)
+	.add('BlockIntro (right)', () => (
+		<BlockIntro title="Page title" content={loremIpsum({ count: 3 })} align="right" />
+	));
