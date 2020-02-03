4caa2dbcd95a7c9d116f087a67eaaad95fff5d7e
Andry Charlier <andry.charlier@studiohyperdrive.be>
2020-02-03 11:49:48 +0100

    fix: replace backticks with single quotes

--- src/content-blocks/BlockAccordions/BlockAccordions.mock.tsx
+++ src/content-blocks/BlockAccordions/BlockAccordions.mock.tsx
@@ -5,6 +5,6 @@ export const ACCORDIONS_MOCK = [
 	{ title: 'FAQ 2', content: '<p>Dolor sit amet</p>' },
 	{
 		title: 'FAQ 3',
-		content: `<p>consectetur <a href="#link">adipisicing</a> elit</p>`,
+		content: '<p>consectetur <a href="#link">adipisicing</a> elit</p>',
 	},
 ];
