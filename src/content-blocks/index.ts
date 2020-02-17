2a7012a135f5ff558ea3ce918c1b1339abe21be2
Andry Charlier <andry.charlier@studiohyperdrive.be>
2020-02-17 11:21:20 +0100

    feat: add separate index for components

--- src/content-blocks/index.ts
+++ src/content-blocks/index.ts
@@ -8,3 +8,19 @@ export { BlockIFrame, BlockIFrameProps } from './BlockIFrame/BlockIFrame';
 export { BlockIntro, BlockIntroProps } from './BlockIntro/BlockIntro';
 export { BlockMediaList, BlockMediaListProps } from './BlockMediaList/BlockMediaList';
 export { BlockRichText, BlockRichTextProps } from './BlockRichText/BlockRichText';
+export { BlockImage, BlockImageProps } from './BlockImage/BlockImage';
+export {
+	BlockImageTitleTextButton,
+	BlockImageTitleTextButtonProps,
+} from './BlockImageTitleTextButton/BlockImageTitleTextButton';
+export { BlockLinks, BlockLinksProps } from './BlockLinks/BlockLinks';
+export { BlockQuote, BlockQuoteProps } from './BlockQuote/BlockQuote';
+export {
+	BlockTitleImageText,
+	BlockTitleImageTextProps,
+} from './BlockTitleImageText/BlockTitleImageText';
+export { BlockVideo, BlockVideoProps } from './BlockVideo/BlockVideo';
+export {
+	BlockVideoTitleTextButton,
+	BlockVideoTitleTextButtonProps,
+} from './BlockVideoTitleTextButton/BlockVideoTitleTextButton';
