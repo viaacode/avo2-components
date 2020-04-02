ed7ae237150db33b90e85f7b97b9ce58da1d11bb
Bert Verhelst <verhelstbert@gmail.com>
2020-04-02 11:36:56 +0200

    fix(button-block): avoid margin left on buttons with divider
    otherwise the button on the left of the divider is closer to the divider than the button on the right of the divider

--- src/content-blocks/BlockButtons/BlockButtons.stories.tsx
+++ src/content-blocks/BlockButtons/BlockButtons.stories.tsx
@@ -2,12 +2,21 @@ import { storiesOf } from '@storybook/react';
 import React from 'react';
 
 import { AlignOptions } from '../../types';
+import { ButtonType } from '../../components';
 
 import { BlockButtons } from './BlockButtons';
 import { BUTTONS_MOCK } from './BlockButtons.mock';
 
-const renderBlockButtons = (align?: AlignOptions, dividers = false) => () => (
-	<BlockButtons elements={BUTTONS_MOCK} align={align} hasDividers={dividers} />
+const renderBlockButtons = (
+	align?: AlignOptions,
+	dividers = false,
+	type: ButtonType = 'secondary'
+) => () => (
+	<BlockButtons
+		elements={BUTTONS_MOCK.map(button => ({ ...button, type: type }))}
+		align={align}
+		hasDividers={dividers}
+	/>
 );
 
 storiesOf('blocks/BlockButtons', module)
@@ -16,4 +25,4 @@ storiesOf('blocks/BlockButtons', module)
 	.add('BlockButtons (left)', renderBlockButtons('left'))
 	.add('BlockButtons (center)', renderBlockButtons('center'))
 	.add('BlockButtons (right)', renderBlockButtons('right'))
-	.add('BlockButtons (dividers)', renderBlockButtons('left', true));
+	.add('BlockButtons (dividers)', renderBlockButtons('left', true, 'link'));
