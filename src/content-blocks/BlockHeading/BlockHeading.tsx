fc698da3bc0c01e65a614770d8edd1404a4aa56c
Bert Verhelst <verhelstbert@gmail.com>
2020-05-05 09:33:56 +0200

    fix(blocks): remove margin on block content, block will handle margin

--- src/content-blocks/BlockHeading/BlockHeading.tsx
+++ src/content-blocks/BlockHeading/BlockHeading.tsx
@@ -23,7 +23,7 @@ export const BlockHeading: FunctionComponent<BlockHeadingProps> = ({
 	<Type
 		className={classnames(className, `c-${Type}`, `u-text-${align}`)}
 		onClick={onClick}
-		style={color ? { color } : {}}
+		style={color ? { color, margin: 0 } : { margin: 0 }}
 	>
 		{children}
 	</Type>
