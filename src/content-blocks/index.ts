8045effb2bcddd3dd58ec5a1114f39d8a1f03733
Bert Verhelst <verhelstbert@gmail.com>
2020-06-12 15:55:37 +0200

    fix(flow-player): remove old reference to flow player block

--- src/content-blocks/index.ts
+++ src/content-blocks/index.ts
@@ -2,7 +2,6 @@
 export { BlockAccordions, BlockAccordionsProps } from './BlockAccordions/BlockAccordions';
 export { BlockButtons, BlockButtonsProps } from './BlockButtons/BlockButtons';
 export { BlockCTAs, BlockCTAsProps } from './BlockCTAs/BlockCTAs';
-export { BlockFlowPlayer, BlockFlowPlayerProps } from './BlockFlowPlayer/BlockFlowPlayer';
 export { BlockGrid, BlockGridProps, GridItem } from './BlockGrid/BlockGrid';
 export { BlockHeading, BlockHeadingProps } from './BlockHeading/BlockHeading';
 export { BlockIFrame, BlockIFrameProps } from './BlockIFrame/BlockIFrame';
