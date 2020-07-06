baa074bbfbe773c1b4257bde0556e8bdb1e20569
Bert Verhelst <verhelstbert@gmail.com>
2020-07-06 12:17:36 +0200

    fix(block-cta): correctly pass navigate function to cta components

--- src/content-blocks/BlockCTAs/BlockCTAs.tsx
+++ src/content-blocks/BlockCTAs/BlockCTAs.tsx
@@ -2,19 +2,26 @@ import classnames from 'classnames';
 import React, { FunctionComponent } from 'react';
 
 import { CTA, CTAProps } from '../../components';
-import { DefaultProps } from '../../types';
+import { ButtonAction, DefaultProps } from '../../types';
 
 export interface BlockCTAsProps extends DefaultProps {
 	elements: CTAProps[];
 	width?: string;
+	navigate: (buttonAction: ButtonAction) => void;
 }
 
-export const BlockCTAs: FunctionComponent<BlockCTAsProps> = ({ className, elements, width }) => (
+export const BlockCTAs: FunctionComponent<BlockCTAsProps> = ({
+	className,
+	elements,
+	width,
+	navigate,
+}) => (
 	<div className={classnames(className, 'c-cta')}>
 		{elements.map((cta, index) => (
 			<CTA
 				key={`cta-${index}`}
 				{...cta}
+				navigate={navigate}
 				width={width || (elements.length === 1 ? '100%' : '50%')}
 			/>
 		))}
