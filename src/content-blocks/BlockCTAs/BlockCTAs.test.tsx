d97e85bc58b31cdc822a5ae628090defbcdf518f
Bert Verhelst <verhelstbert@gmail.com>
2020-08-07 09:46:37 +0200

    fix(block-cta): fix tsc errors with cta and media list blocks

--- src/content-blocks/BlockCTAs/BlockCTAs.test.tsx
+++ src/content-blocks/BlockCTAs/BlockCTAs.test.tsx
@@ -1,29 +1,34 @@
 import { mount, shallow } from 'enzyme';
 import React from 'react';
+import { action } from '../../helpers';
 
 import { BlockCTAs } from './BlockCTAs';
 import { CTAS_MOCK } from './BlockCTAs.mock';
 
+const navigate = action('navigate to');
+
 describe('<BlockCTAs />', () => {
 	it('Should be able to render', () => {
-		shallow(<BlockCTAs elements={CTAS_MOCK} />);
+		shallow(<BlockCTAs elements={CTAS_MOCK} navigate={navigate} />);
 	});
 
 	it('should pass a custom className', () => {
 		const customClass = 'c-cta-custom';
-		const CTAsComponent = <BlockCTAs className={customClass} elements={CTAS_MOCK} />;
+		const CTAsComponent = (
+			<BlockCTAs className={customClass} elements={CTAS_MOCK} navigate={navigate} />
+		);
 
 		expect(shallow(CTAsComponent).hasClass(customClass)).toBeTruthy();
 	});
 
 	it('Should correctly pass on elements prop', () => {
-		const CTAsComponent = mount(<BlockCTAs elements={CTAS_MOCK} />);
+		const CTAsComponent = mount(<BlockCTAs elements={CTAS_MOCK} navigate={navigate} />);
 
 		expect(CTAsComponent.prop('elements')).toEqual(CTAS_MOCK);
 	});
 
 	it('Should correctly render CTAs', () => {
-		const CTAsComponent = mount(<BlockCTAs elements={CTAS_MOCK} />);
+		const CTAsComponent = mount(<BlockCTAs elements={CTAS_MOCK} navigate={navigate} />);
 		const CTAsBlock = CTAsComponent.find('.c-cta');
 		const CTAs = CTAsBlock.find('.c-cta-item');
 
