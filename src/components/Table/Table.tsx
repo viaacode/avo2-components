2ba2cb4def0d26225e549dc6938baa6ea1b35feb
Bert Verhelst <verhelstbert@gmail.com>
2020-08-10 16:16:30 +0200

    fix(table): remove unused lodash "without" import

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,5 +1,5 @@
 import classnames from 'classnames';
-import { every, without } from 'lodash-es';
+import { every } from 'lodash-es';
 import React, { FunctionComponent, ReactNode } from 'react';
 
 import { DefaultProps } from '../../types';
