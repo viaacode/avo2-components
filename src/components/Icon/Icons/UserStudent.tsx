08b708c970ca6b9945f3724ffa9692176131bb1a
Ian Emsens <ian.emsens@studiohyperdrive.be>
2022-02-01 10:19:43 +0100

    fix(avo-1829): correct student icon and add currentColor fill to student and teach

--- src/components/Icon/Icons/UserStudent.tsx
+++ src/components/Icon/Icons/UserStudent.tsx
@@ -1,24 +1,22 @@
 import React, { SVGProps } from 'react';
 
 const UserStudent = (props: SVGProps<SVGSVGElement>) => (
-	<svg width={17} height={21} viewBox="0 0 17 21" fill="none" {...props}>
+	<svg fill="none" viewBox="0 0 17 21" {...props}>
 		<path
-			d="M11.534 8.07c0 1.658-1.377 3.036-3.12 3.036-1.742 0-3.119-1.378-3.119-3.035 0-1.657 1.377-3.036 3.12-3.036 1.742 0 3.119 1.379 3.119 3.036z"
-			fill="#fff"
-			stroke="#2B414F"
-			strokeWidth={2}
+			fill="currentColor"
+			fillRule="evenodd"
+			d="M5.824 15.123a3 3 0 00-3 3v1.65a1 1 0 11-2 0v-1.65a5 5 0 015-5h5.182a5 5 0 015 5v1.65a1 1 0 11-2 0v-1.65a3 3 0 00-3-3H5.824z"
+			clipRule="evenodd"
 		/>
 		<path
-			d="M1.824 19.773v-1.65a4 4 0 014-4h5.182a4 4 0 014 4v1.65"
-			stroke="#2B414F"
-			strokeWidth={2}
-			strokeLinecap="round"
+			fill="currentColor"
+			d="M4.813 6.11a3.953 3.953 0 00-.518 1.96c0 2.23 1.845 4.036 4.12 4.036 2.275 0 4.119-1.807 4.119-4.035 0-.712-.188-1.38-.518-1.96l-1.758.957c.177.3.276.644.276 1.003 0 1.085-.91 2.035-2.12 2.035-1.209 0-2.119-.95-2.119-2.035 0-.36.1-.704.277-1.003L4.813 6.11z"
 		/>
 		<path
-			d="M3.092 4.035l5.323-2.897 5.323 2.897-5.323 2.897-5.323-2.897z"
-			fill="#fff"
-			stroke="#2B414F"
-			strokeWidth={2}
+			fill="currentColor"
+			fillRule="evenodd"
+			d="M8.415 2.277L5.184 4.035l3.23 1.758 3.232-1.758-3.231-1.758zM8.654.13a.5.5 0 00-.478 0l-6.37 3.466a.5.5 0 000 .878l6.37 3.466a.5.5 0 00.478 0l6.369-3.466a.5.5 0 000-.878L8.652.13z"
+			clipRule="evenodd"
 		/>
 	</svg>
 );
