cd79885236daabc230d2a10422e25044be8ba94d
Ian Emsens <ian.emsens@studiohyperdrive.be>
2022-06-08 18:05:33 +0200

    Revert "feat: adjust typing"
    This reverts commit 5108c0008cab3abc958e464e1a849aa2fa06dfbe.

--- src/components/Tabs/Tab/Tab.tsx
+++ src/components/Tabs/Tab/Tab.tsx
@@ -1,5 +1,5 @@
 import classnames from 'classnames';
-import React, { FunctionComponent, ReactNode } from 'react';
+import React, { FunctionComponent } from 'react';
 
 import { DefaultProps } from '../../../types';
 import { Icon } from '../../Icon/Icon';
@@ -8,7 +8,7 @@ import { IconNameSchema } from '../../Icon/Icon.types';
 import './Tab.scss';
 
 export interface TabPropsSchema extends DefaultProps {
-	label: ReactNode;
+	label: string;
 	id: string | number;
 	icon?: IconNameSchema;
 	active?: boolean;
