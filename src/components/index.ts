2d7d8416ad1f88f0e8442753596a77bf73fe7a4b
Ian Emsens <ian.emsens@studiohyperdrive.be>
2022-06-22 16:58:00 +0200

    fix(avo-1978): update barrel

--- src/components/index.ts
+++ src/components/index.ts
@@ -18,6 +18,7 @@ import { ContainerPropsSchema } from './Container/Container';
 import { ContentInputPropsSchema } from './ContentInput';
 import { CTAPropsSchema } from './CTA/CTA';
 import { DatePickerPropsSchema } from './DatePicker/DatePicker';
+import { DeadlineIndicatorPropsSchema } from './DeadlineIndicator/DeadlineIndicator';
 import { DropdownPropsSchema } from './Dropdown/Dropdown';
 import { ExpandableContainerPropsSchema } from './ExpandableContainer/ExpandableContainer';
 import { FlexPropsSchema } from './Flex/Flex';
@@ -312,3 +313,6 @@ export type ContentInputProps = ContentInputPropsSchema;
 
 export { StickyEdgeBar } from './StickyEdgeBar/StickyEdgeBar';
 export type StickyEdgeBarProps = StickyEdgeBarPropsSchema;
+
+export { DeadlineIndicator } from './DeadlineIndicator/DeadlineIndicator';
+export type DeadlineIndicatorProps = DeadlineIndicatorPropsSchema;
