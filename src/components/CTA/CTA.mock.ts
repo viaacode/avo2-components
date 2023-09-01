b572e318ec57dede1f28ce44ce766f6e4b3153ef
Bert Verhelst <verhelstbert@gmail.com>
2023-09-01 13:50:08 +0200

    fix(avo-2877): move content picker type to typings
    https://meemoo.atlassian.net/browse/AVO-2877

--- src/components/CTA/CTA.mock.ts
+++ src/components/CTA/CTA.mock.ts
@@ -1,6 +1,7 @@
 import { action } from '@storybook/addon-actions';
+import { ContentPickerType } from '@viaa/avo2-types';
 
-import { testRenderLink } from '../../helpers/render-link';
+import { testRenderLink } from '../../helpers';
 import { LinkTarget } from '../../types';
 import { IconNameSchema } from '../Icon/Icon.types';
 
@@ -14,7 +15,7 @@ export const CTA_MOCK: CTAPropsSchema = {
 	buttonIcon: IconNameSchema.chevronDown,
 	buttonAction: {
 		target: LinkTarget.Blank,
-		type: 'EXTERNAL_LINK',
+		type: ContentPickerType.EXTERNAL_LINK,
 		value: 'http://google.com',
 	},
 	renderLink: testRenderLink(action('navigate to')),
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,4 @@
+import { ContentPickerType } from '@viaa/avo2-types';
 import { CSSProperties, ReactNode } from 'react';
 
 export * from './content-type';
@@ -12,20 +13,6 @@ export type VideoAspectRatio = '3:2' | '16:9';
 export type HeadingType = 'h1' | 'h2' | 'h3' | 'h4';
 
 export type AlignOptions = 'left' | 'center' | 'right';
-
-export type ContentPickerType =
-	| 'CONTENT_PAGE'
-	| 'COLLECTION'
-	| 'ITEM'
-	| 'BUNDLE'
-	| 'DROPDOWN'
-	| 'INTERNAL_LINK'
-	| 'EXTERNAL_LINK'
-	| 'SEARCH_QUERY'
-	| 'PROJECTS'
-	| 'ANCHOR_LINK'
-	| 'PROFILE'
-	| 'FILE'; // TODO move to typings and make typings repo a dependency of the components repo
 
 export enum LinkTarget {
 	Self = '_self',
